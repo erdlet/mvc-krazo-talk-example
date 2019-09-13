c3de288accaf6600d0a6246bf3660ebf4fdd9703
Erdle, Tobias <tobias.erdle@innoq.com>
2019-09-13 12:09:21 +0200

    Add hint for IMPLICIT CSRF protection

--- src/main/java/de/erdlet/preview/mvc/App.java
+++ src/main/java/de/erdlet/preview/mvc/App.java
@@ -1,8 +1,21 @@
 package de.erdlet.preview.mvc;
 
+import java.util.Map;
+import javax.mvc.security.Csrf;
+import javax.mvc.security.Csrf.CsrfOptions;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/mvc")
 public class App extends Application {
+//
+//  Enables IMPLICIT CSRF protection. This means, that the @CsrfProtected annotation
+//  doesn't need to be set on controllers.
+//
+//  Nevertheless, the hidden field or HTTP header has to be added manually.
+//
+//  @Override
+//  public Map<String, Object> getProperties() {
+//    return Map.of(Csrf.CSRF_PROTECTION, CsrfOptions.IMPLICIT);
+//  }
 }
